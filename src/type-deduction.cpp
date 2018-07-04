1b2b4cf2b7817c268d9d0709cd2602382222dac7
~zw <zeppelinwave@gmail.com>
2018-07-04 17:51:51 +0100

    added type-deduction.cpp

---
+++ src/type-deduction.cpp
@@ -0,0 +1,57 @@
+/*
+ * type-deduction.cpp
+ * examples from Item 1: Understand template type deduction, from
+ * Effective Modern C++
+ */
+#include <iostream>
+#include <vector>
+
+template<typename T>
+bool is_in(const std::vector<T>& v, const T& item)
+{
+  for(auto& a: v)
+    if(a == item)
+      return true;
+  return false;
+}
+
+template<typename T>
+void update_at_index(std::vector<T>& v, const T* item, int index)
+{
+  auto i {0};
+  for(auto& a: v){
+    if(i == index)
+      a = *item;
+    i++;
+  }
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v)
+{
+  os << "{ ";
+  for(auto& a: v){
+    os << a << ",";
+  }
+  os << " }";
+  return os;
+}
+
+int main()
+{
+  std::vector<int> vi {2,4,8,10,12};
+  int a=13;
+  int b=12;
+  int& c = b;
+  std::cout << ((is_in(vi, 13)) ? "yes" : "no") << std::endl;
+  //it is only ok to call is_in with 13 if it is expecting a const T&
+  //const values are lvalues (a temporary variable is created)
+  std::cout << ((is_in(vi, c)) ? "yes" : "no") << std::endl;
+  //type deduction, case 1 (reference but not a universal ref)
+  //v's type is therefore deduced to be const vector<int>&
+  //T is type const vector<int>
+  //when 'c' is passed, it's 'referenceness' is ignored during type deduction
+  //ie T is not deducted to be const int&
+  std::cout << vi << std::endl;
+  return 0;
+}
